6c97a2e3c58c1654d1a7404b948bf3e43a9fcb8c
Ahmedi7a <ahmedamralla73@email.com>
2025-02-18 16:42:05 +0300

    done

---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,18 @@
+
+function IngredientList(props) {
+    return (
+        <ul>
+
+            {props.availableIngredients.map((ingredient) => (
+                <li 
+                style={{backgroundColor: ingredient.color}}
+                onClick={()=>props.handleAddToStack(ingredient)}
+                >
+                    {ingredient.name}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default IngredientList;
